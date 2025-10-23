2bd343dd310bf79da930bdeb382e028677a3271a
Roberto Moreira <robertodev04@gmail.com>
2025-10-23 17:15:33 -0300

    modelos da api

---
+++ fast_car_api/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Text
+
+from fast_car_api.database import Base
+
+
+class Car(Base):
+    __tablename__ = 'cars'
+
+    id = Column(Integer, primary_key=True, index=True)
+    brand = Column(String, nullable=False)
+    model = Column(String, nullable=False)
+    color = Column(String, nullable=True)
+    factory_year = Column(Integer, nullable=True)
+    description = Column(Text, nullable=True)
+
