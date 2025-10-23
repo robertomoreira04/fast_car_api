9063b49c9c0b5f19636e231e1295ed2e86955d45
Roberto Moreira <robertodev04@gmail.com>
2025-10-23 17:15:15 -0300

    rota principal da api

---
+++ fast_car_api/app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fast_car_api.routers import router as car_router
+app = FastAPI(
+    title='Fast API Cars',
+    version='0.1.0',
+)
+
+app.include_router(car_router)
+
+@app.get('/')
+def read_root():
+    return {'status': 'ok'}
