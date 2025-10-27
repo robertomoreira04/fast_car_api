33d786ba872361c72fd0093389c00cc9c7177418
Roberto Moreira <robertodev04@gmail.com>
2025-10-27 14:59:15 -0300

    rota de criação de carros

--- fast_car_api/routers.py
+++ fast_car_api/routers.py
@@ -1,17 +1,13 @@
 from fastapi import APIRouter
 
+from fast_car_api.schemas import CarSchema
+
 router = APIRouter(
     prefix='/api/v1/cars',
     tags=['cars'],
 )
 
 
-@router.get('/')
-def list_cars():
-    return {
-        'cars': [
-            {'id': 1, 'modelo': 'Peugeot 206'},
-            {'id': 2, 'modelo': 'Lancer 2014'},
-            {'id': 3, 'modelo': 'Corolla 2025'},
-        ]
-    }
+@router.post('/')
+def create_car(car: CarSchema):
+    return car
