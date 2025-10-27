9367dcca6ec4ee1aff398482cb5b5516a42b2e0b
Roberto Moreira <robertodev04@gmail.com>
2025-10-27 15:10:15 -0300

    criando schema publico de carro

--- fast_car_api/schemas.py
+++ fast_car_api/schemas.py
@@ -8,3 +8,12 @@ class CarSchema(BaseModel):
     factory_year: Optional[int] = None
     model_year: Optional[int] = None
     description: Optional[str] = None
+
+class CarPublic(BaseModel):
+    id: int
+    brand: str
+    model: str 
+    color: str
+    factory_year: int
+    model_year: int
+    description: str
