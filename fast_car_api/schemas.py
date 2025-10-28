b9dd1200cd219c7609f74f8735c03f4a10c5e643
Roberto Moreira <robertodev04@gmail.com>
2025-10-28 16:16:46 -0300

    mudando esquema do car public

--- fast_car_api/schemas.py
+++ fast_car_api/schemas.py
@@ -13,7 +13,10 @@ class CarPublic(BaseModel):
     id: int
     brand: str
     model: str 
-    color: str
-    factory_year: int
-    model_year: int
-    description: str
+    color: Optional[str] = None
+    factory_year: Optional[int] = None
+    model_year: Optional[int] = None
+    description: Optional[str] = None
+
+class CarList(BaseModel):
+    cars: list[CarPublic]
