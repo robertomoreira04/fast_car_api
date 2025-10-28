ae7d2e1c1942e6fda0da236556571cf598b7d0f8
Roberto Moreira <robertodev04@gmail.com>
2025-10-28 16:07:33 -0300

    criando rota de criação de carro linkado ao banco de dados

--- fast_car_api/routers.py
+++ fast_car_api/routers.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
 
+from fast_car_api.models import Car
+from fast_car_api.database import get_session
 from fast_car_api.schemas import CarPublic, CarSchema
 
 router = APIRouter(
@@ -9,9 +12,13 @@ router = APIRouter(
 
 
 @router.post(
-        '/', 
+        path='/', 
         response_model=CarPublic, 
         status_code=status.HTTP_201_CREATED
 )
-def create_car(car: CarSchema):
+def create_car(car: CarSchema,session: Session = Depends(get_session)):
+    car = Car(**car.model_dump())
+    session.add(car)
+    session.commit()
+    session.refresh(car)
     return car
