06094e0ae1538b36375439aff39eebc43791fe4d
Roberto Moreira <robertodev04@gmail.com>
2025-10-28 16:16:57 -0300

    adicionando rota de lista de carros

--- fast_car_api/routers.py
+++ fast_car_api/routers.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends, status
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from fast_car_api.models import Car
 from fast_car_api.database import get_session
-from fast_car_api.schemas import CarPublic, CarSchema
+from fast_car_api.schemas import CarPublic, CarSchema, CarList
 
 router = APIRouter(
     prefix='/api/v1/cars',
@@ -22,3 +23,14 @@ def create_car(car: CarSchema,session: Session = Depends(get_session)):
     session.commit()
     session.refresh(car)
     return car
+
+
+@router.get(
+    path='/',
+    response_model=CarList,
+    status_code=status.HTTP_200_OK,
+)
+def list_cars(session: Session = Depends(get_session),):
+    query = session.scalars(select(Car))
+    cars = query.all()
+    return {'cars': cars}
